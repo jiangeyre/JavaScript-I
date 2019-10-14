549ae8f47b774e8cfb6b8e92861680e66377bd01
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-10-14 14:13:02 -0700

    all mvp done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,47 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    return "Function was invoked!";
+}
+console.log(myFunction());
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+    return param;
+}
+
+console.log(anotherFunction("Example"));
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+}
+
+console.log(add(1,2));
+
+
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (param1, param2) => {
+    return param1 - param2;
+}
+
+console.log(subtract(666, 420));
 
 
 // Stretch
@@ -28,3 +55,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1, 2, 3, 4];
+
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
